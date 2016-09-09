04058015c2e30d8415114cc420a79d8986c8d93d
ostridgep <paul@ostridge.com>
2016-09-09 12:31:34 +0100

    xx

--- PGHostApp/WebContent/js/index.js
+++ PGHostApp/WebContent/js/index.js
@@ -91,7 +91,7 @@ function startApp(){
 	});
 }
 function startApp1(){
-	ref=window.open('myjobschild://?MYJOBS=xx','_system')
+	ref=cordova.InAppBrowser.open('myjobschild://?MYJOBS=xx','_system')
 	ref.addEventListener('loadstart',function(event) { alert("loadstart"+event.url); });
 	ref.addEventListener('loadstop',function(event) { alert("loadstop"+event.url); });
 	ref.addEventListener('exit',function(event) { alert("exit"+event.url); });
